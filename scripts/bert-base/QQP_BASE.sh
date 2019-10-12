a6d8d1df7da3a2404cbaf8577b481946f46ecaa2
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-10-12 18:13:03 -0400

    missing scripts

---
+++ scripts/bert-base/QQP_BASE.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=0-08:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8G
+
+source ~/ENV/bin/activate
+
+TASK='QQP'
+SEED=$1
+
+bash scripts/bert-base/finetune.sh $TASK "BASE" $SEED
+
+deactivate
---
+++ scripts/bert-large/QNLI_NONE.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=0-18:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8G
+
+source ~/ENV/bin/activate
+
+TASK='QNLI'
+SEED=$1
+
+bash scripts/bert-large/finetune.sh $TASK "NONE" $SEED
+
+deactivate
