20f98520b5929c2571aeb8694261166e682b5265
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-10-15 12:07:50 -0400

    minor code clean up

--- scripts/roberta-large/MNLI_BASE.sh
+++ scripts/roberta-large/MNLI_BASE.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=0-18:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
-#SBATCH --output=roberta-large-MNLI-large.out
+#SBATCH --output=roberta-large-MNLI-base.out
 #SBATCH --mem=64G
 
 source ~/ENV/bin/activate
