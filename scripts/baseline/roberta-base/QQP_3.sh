5bdc06d392759beb2936a6d9037b69a400b4cdde
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-10-17 16:50:48 -0400

    roberta baseline

---
+++ scripts/baseline/roberta-base/QQP_3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=def-jimmylin
+#SBATCH --time=1-00:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=baseline-roberta-base-QQP_3.out
+#SBATCH --mem=64G
+
+source ~/ENV/bin/activate
+
+TASK='QQP'
+SEED=$1
+
+bash scripts/baseline/roberta-base.sh $TASK $SEED 3e-5
+
+deactivate
---
+++ scripts/baseline/roberta-base/SST-2_4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=def-jimmylin
+#SBATCH --time=0-10:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=baseline-roberta-base-SST-2_4.out
+#SBATCH --mem=64G
+
+source ~/ENV/bin/activate
+
+TASK='SST-2'
+SEED=$1
+
+bash scripts/baseline/roberta-base.sh $TASK $SEED 4e-5
+
+deactivate
