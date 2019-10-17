72a889c85e862b4864b3dc602e572a7c6ccda68a
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-10-17 17:31:10 -0400

    bash scripts for generating ralph scripts

---
+++ generate_ralph_scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp -rf scripts ralph_scripts
+cd ralph_scripts
+
+find ./ -type f -exec sed -i -e 's/source ~\/ENV\/bin\/activate//g' {} \;
+
+find ./ -type f -exec sed -i -e 's/deactivate//g' {} \;
+
+find ./ -type f -exec sed -i -e 's/jimmylin/lilimou/g' {} \;
+
+cd ..
